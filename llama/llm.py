3b722b5417afd2f7985f85bba35df1e7b16cbdd1
Antonio Sousa <139799877+antoniosousa1@users.noreply.github.com>
2024-11-01 11:06:38 -0400

    working directory loader rettiever needs further testing (#5)

--- llama/llm.py
+++ llama/llm.py
@@ -1,5 +1,5 @@
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 
@@ -7,8 +7,9 @@ from langchain_chroma import Chroma
 llm = OllamaLLM(model="llama3.1")
 
 #loads data from markdown file
-loader = TextLoader(
-    "./data/beeMovie.txt"
+loader = DirectoryLoader(
+    "./data",
+    loader_cls=TextLoader
 )
 
 #assigns docs to the loaded documents
@@ -24,7 +25,7 @@ docs = loader.load()
 
 #splits the text into 1000 char chunks with a 150 char overlap to not cut off important context, also text is split by an empty line aswell
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=150, add_start_index=True
+    chunk_size=250, chunk_overlap=100, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
 
@@ -36,17 +37,23 @@ llamaEmbeddings = OllamaEmbeddings(
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=llamaEmbeddings)
 
 #retrives 10 documents that meet search parameters of similar
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+question = input()
 
 #prompt for serching avalible docuemnts
-retrieved_docs = retriever.invoke("what happens to the flowers in the movie")
+retrieved_docs = retriever.invoke(question)
+
+for i in retrieved_docs:
+    print(i.page_content)
+    print("\n")
 
 #function to create a prompt from a predetermined prompt format and the context given from the retriver
 def create_prompt():
 
     prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer he question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\n"
     
-    Question = "what happens to the flowers in the movie\n"
+    Question = "Question: " + question + "\n"
 
     Context_str = "Context: \n\n"
 
