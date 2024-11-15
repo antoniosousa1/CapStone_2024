37806c32d58038a33388f2c7499beb66982ed0c9
MattG611 <mgreeson11@gmail.com>
2024-11-15 10:04:03 -0500

    Cleaned up code

--- Integrated_llama_RAG/llm.py
+++ Integrated_llama_RAG/llm.py
@@ -5,30 +5,31 @@ from langchain_chroma import Chroma
 from langchain_community.vectorstores import Chroma
 import os
 
-#sets llama 3.1 as the llm a varable
+# Sets llama 3.1 as the llm a varable
 llm = OllamaLLM(model="llama3.1:70b")
 
-#loads data from markdown file
+# Loads data from markdown file
 loader = DirectoryLoader(
     path="./data"
 )
 
-#assigns docs to the loaded documents
+# Assigns docs to the loaded documents
 docs = loader.load()
 
-#splits the text into 1000 char chunks with a 150 char overlap to not cut off important context, also text is split by an empty line aswell
+# Splits the text into 1000 char chunks with a 150 char overlap to not cut off important context, also text is split by an empty line aswell
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=250, chunk_overlap=100, add_start_index=True
 )
 
+# Splits the documents up and stores the chuncks of text into all_splits
 all_splits = text_splitter.split_documents(docs)
 
-#chooses which model of ollama embeddings to use
+# Chooses which model of ollama embeddings to use
 llamaEmbeddings = OllamaEmbeddings(
     model="llama3.1:70b"
 )
 
-#The path to store the vector (pwd for your own path)
+# The path to store the vector (pwd for your own path)
 vectorstore_path = "/x1/mgreeson/WorkFolder/CapStoneProject/CapStone_2024/Integrated_llama_RAG/Vector_Storage_Data" 
 
 # If the vector data has been stored properly, load it and check if data actually exists in it 
@@ -36,20 +37,22 @@ if os.path.exists(vectorstore_path) and os.listdir(vectorstore_path):
     # Entered the if statement, start loading vector database
     print("Loading the existing vector data...")
     
-    # Try to load it, if not throw an error 
+    # Try and except, handles any errors that might occur if the database doesn't load properly 
     try:
-        # Load the vectorstore from the persisted directory
+        # Load the vector database from the persisted directory
         vectorstore = Chroma(persist_directory=vectorstore_path, embedding_function=llamaEmbeddings) 
         
+        # Tells user that the database was loaded successfully
         print("Vector data loaded successfully.")
 
     # Exception that throws an error when loading the database
-    except Exception as e:
-        print(f"Error loading vector data: {e}")
-# else that runs if the vector data hasn't been created and stored 
+    except:
+        print("Error loading vector database")
+
+# Else that runs if the vector data hasn't been created and stored 
 else:
     # Print that tells user the data is being stored
-    print("Creating and Storing vector database data...")
+    print("Creating and Storing vector database...")
 
     # Creates vectore database with llama embeddings
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=llamaEmbeddings, persist_directory=vectorstore_path)
@@ -57,24 +60,24 @@ else:
     # Tells the user that the vector database data has been stored and persisted 
     print("Storage of vector data created and persisted.")
 
-#retrives 10 documents that meet search parameters of similar
+# Retrives 10 documents that meet search parameters of similar
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
-#Text doesn't display to the user till all the documents have been split and stored
-#Asks the user to input a question and stores the users question into the question variable 
+# Text doesn't display to the user till all the documents have been split and stored
+# Asks the user to input a question and stores the users question into the question variable 
 question = input("What is your question: ")
 
-#prompt for serching avalible docuemnts
+# Prompt for serching avalible docuemnts
 retrieved_docs = retriever.invoke(question)
 
-#Outputs the chunks of text to the terminal that are split up
+# Outputs the chunks of text to the terminal that are split up
 '''
 for i in retrieved_docs:
     print(i.page_content)
     print("BREAK\n")
 '''
 
-#function to create a prompt from a predetermined prompt format and the context given from the retriver
+# Function to create a prompt from a predetermined prompt format and the context given from the retriver
 def create_prompt():
 
     prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer he question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\n"
@@ -92,7 +95,7 @@ def create_prompt():
 
     return final_prompt
 
-#print the respone from the ollama llm that was given the formated prompt
+# Print the respone from the ollama llm that was given the formated prompt
 print("\n")
 print("Answer: " + llm.invoke(create_prompt()))
 print("\n")
