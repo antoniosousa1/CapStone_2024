1866fc92028c5e9c8fdce87fe3906aede36924fb
Antonio Sousa <139799877+antoniosousa1@users.noreply.github.com>
2024-11-19 15:50:58 -0500

    Feature docker file (#8)
    * dockerfile now works in creating the correct image just need to get it to listen to ollama using docker compose
    
    * docker compose works with small workload need to get kubernetes running to autoscale resoures
    
    * got docker image working with locally installed ollama, ollama docker image only works with nividia or amd gpu whilelocal client will work with any gpu
    
    * finished docker file image now works when ollama is running locally
    
    * Made changes to the question to take input and changed readme

--- App/llm.py
+++ App/llm.py
@@ -3,8 +3,15 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 
+import os
+
+
+# Access the Ollama server URL
+ollama_server_url = os.getenv("OLLAMA_SERVER_URL")
+print(f"Using Ollama server at: {ollama_server_url}")
+
 #sets llama 3.1 as the llm a varable
-llm = OllamaLLM(model="llama3.1:70b")
+llm = OllamaLLM(model="llama3.1", base_url=ollama_server_url)
 
 #loads data from markdown file
 loader = DirectoryLoader(
@@ -13,31 +20,35 @@ loader = DirectoryLoader(
 
 #assigns docs to the loaded documents
 docs = loader.load()
+
+print("passed loader")
 #splits the text into 1000 char chunks with a 150 char overlap to not cut off important context, also text is split by an empty line aswell
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=250, chunk_overlap=100, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
 
+print("passed text splitter")
 #chooses which model of ollama embeddings to use
 llamaEmbeddings = OllamaEmbeddings(
-    model="llama3.1:70b"
+    model="llama3.1", base_url=ollama_server_url
 )
+
 #creates vectore database with llama embeddings
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=llamaEmbeddings)
 
-#retrives 10 documents that meet search parameters of similar
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+print("paased vectore store and embeddings")
 
+#retrives 10 documents that meet search parameters of similar
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+
+print("please enter question: ")
 question = input()
 
 #prompt for serching avalible docuemnts
 retrieved_docs = retriever.invoke(question)
 
-for i in retrieved_docs:
-    print(i.page_content)
-    print("BREAK\n")
-
+print("retrived docs")
 #function to create a prompt from a predetermined prompt format and the context given from the retriver
 def create_prompt():
 
--- README.md
+++ README.md
@@ -4,10 +4,14 @@ This is the repo for our senior capstone, Generating Content using LLMs
 # LangChain Documentation
 https://python.langchain.com/docs/tutorials/rag/
 
-# LLama 3.1 RAG Setup
-1. Download Ollama from https://ollama.com/ follow instructions in applicaion
-2. When prompted run "ollama pull llama3.1" instead of llama 3.2 in terminal
-3. Ensure the Ollama server is running by opening Ollama application
-4. Run "pip3 install -r requirements.txt"
-5. Run "python3 llm.py" in the llama directory
-6. Note: the application take a while to work so if you want to ensure its working open activity monior or task manager to see how much memory you are using
+# Ollama set up
+1. Visit https://ollama.com/download and install Ollama for your given OS
+2. Once Ollama is install and running, run "ollama pull llama3.1:8b" or "ollama pull llama3.1:70b" this will pull down the given model
+3. Run "ollama list" to see the installed models
+
+# Docker set up
+1. Install docker https://docs.docker.com/engine/install/ and login/create account
+2. Cd into "App" directory
+3. Run the following command to build the image "docker build -t ollama-app ."
+4. After image is created run the following command "docker run -it ollama-app"
+5. You are now in the docker container control+d to exit
