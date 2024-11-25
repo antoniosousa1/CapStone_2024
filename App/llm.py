8f0295ec07652f484a162bc960431cae3c4ab433
antoniosousa1 <antoniosousa640@gmail.com>
2024-11-25 08:48:04 -0500

    added typing to all functions

--- App/llm.py
+++ App/llm.py
@@ -2,16 +2,18 @@ from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
+from langchain.schema import Document
+from langchain.vectorstores.base import VectorStoreRetriever
 import os
 
 # loads the data into usable docs
-def load_docs(data_path):
+def load_docs(data_path: str) -> list[Document]:
     loader = DirectoryLoader(path=data_path)
     docs = loader.load()
     return docs
 
 # splits docs into chunks
-def split_text(docs):
+def split_text(docs: list[Document]) -> list[Document]:
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=250, chunk_overlap=100, add_start_index=True
     )
@@ -19,28 +21,28 @@ def split_text(docs):
     return splits
 
 # creates vector database with llama embeddings
-def create_vector_db(splits, llama_embeddings):
+def create_vector_db(splits: list[Document], llama_embeddings: OllamaEmbeddings) -> Chroma:
     vector_db = Chroma.from_documents(documents=splits, embedding=llama_embeddings)
     return vector_db
 
 # creates retriever
-def create_retriever(vector_db):
+def create_retriever(vector_db: Chroma) -> VectorStoreRetriever:
     retriever = vector_db.as_retriever(search_type="similarity", search_kwargs={"k": 5})
     return retriever
 
 # gets the user question
-def get_user_question():
+def get_user_question() -> str:
     print("Please enter a question: ")
     question = input()
     return question
 
 # retrieves the chunks relevant to question
-def retrieve_docs(retriever, question):
+def retrieve_docs(retriever: VectorStoreRetriever, question: str) -> list[Document]:
     retrieved_docs = retriever.invoke(question)
     return retrieved_docs
 
 # create a prompt
-def create_prompt(retrieved_docs, question):
+def create_prompt(retrieved_docs: list[Document], question: str) -> str:
     prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\n"
     
     Question = "Question: " + question + "\n"
@@ -57,7 +59,7 @@ def create_prompt(retrieved_docs, question):
     return final_prompt
 
 # print the response from the Ollama LLM that was given the formatted prompt
-def get_answer(llm, prompt):
+def get_answer(llm: OllamaLLM, prompt: str) -> str:
     answer = llm.invoke(prompt)
     return answer
 
