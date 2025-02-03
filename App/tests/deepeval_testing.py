f6ba4e79a85b4cbc2117cddd7321217c45c1669c
antoniosousa1 <antoniosousa640@gmail.com>
2025-02-03 10:29:35 -0500

    testing deepeval on 70b parameter model

--- App/tests/deepeval_testing.py
+++ App/tests/deepeval_testing.py
@@ -1,94 +1,26 @@
 from deepeval.synthesizer import Synthesizer
-from typing import List
 from deepeval.synthesizer.config import ContextConstructionConfig
-from deepeval.models.base_model import DeepEvalBaseLLM
-from deepeval.models import DeepEvalBaseEmbeddingModel
-from langchain_ollama import ChatOllama, OllamaEmbeddings
 
 """
-deepeval set-local-model --model-name=<model_name> \
+deepeval set-local-model --model-name=llama3.1:latest \
     --base-url="http://localhost:11434/v1/" \
     --api-key="ollama"
 
 deepeval unset-local-model
 
 
-deepeval set-local-embeddings --model-name=<embedding_model_name> \
+deepeval set-local-embeddings --model-name=llama3.1:latest \
     --base-url="http://localhost:11434/v1/" \
     --api-key="ollama"
 
 deepeval unset-local-embeddings
 """
 
-class LlamaLLM(DeepEvalBaseLLM):
 
-    def __init__(self, model):
-        self.model = model
+synthesizer = Synthesizer()
 
-    def load_model(self):
-        return self.model
-    
-    def generate(self, prompt: str) -> str:
-        chat_model = self.load_model()
-        return chat_model.invoke(prompt).content
-    
-    async def a_generate(self, prompt: str) -> str:
-        chat_model = self.load_model()
-        res = await chat_model.ainvoke(prompt)
-        return res.content
-    
-    def get_model_name(self):
-        return "Custom LLama Model"
-    
+#requires minium of 3 chunks so you must adjust chunk size accordingly
+synthesizer.generate_goldens_from_docs(document_paths=["./data/dog.txt"], context_construction_config=ContextConstructionConfig(chunk_size=50))
 
-class LlamaEmbedder(DeepEvalBaseEmbeddingModel):
-    
-    def __init__(self):
-        pass
-
-    def load_model(self):
-        return OllamaEmbeddings(
-            model="llama3.1:latest",
-            base_url="http://localhost:11434/"
-        )
-    
-    def embed_text(self, text: str) -> List[float]:
-        embedding_model = self.load_model()
-        return embedding_model.embed_query(text)
-
-    def embed_texts(self, texts: List[str]) -> List[List[float]]:
-        embedding_model = self.load_model()
-        return embedding_model.embed_documents(texts)
-
-    async def a_embed_text(self, text: str) -> List[float]:
-        embedding_model = self.load_model()
-        return await embedding_model.aembed_query(text)
-
-    async def a_embed_texts(self, texts: List[str]) -> List[List[float]]:
-        embedding_model = self.load_model()
-        return await embedding_model.aembed_documents(texts)
-
-    def get_model_name(self):
-        "Custom LLama Embedding Model"
-    
-
-custom_model_llama = ChatOllama(
-    model="llama3.1:latest",
-    base_url="http://localhost:11434/",
-    api_key="ollama"
-)
-
-
-llama_model = LlamaLLM(custom_model_llama)
-
-llama_embedding = LlamaEmbedder()
-
-answer = llama_model.generate("how are you")
-
-print(answer)
-
-synthesizer = Synthesizer(model=llama_model)
-
-synthesizer.generate_goldens_from_docs(document_paths=["./data/dog.txt"], context_construction_config=ContextConstructionConfig(chunk_size=100, embedder=llama_embedding))
 
 print(synthesizer.synthetic_goldens)
--- App/tests/ragas_testing.py
+++ App/tests/ragas_testing.py
@@ -4,22 +4,33 @@ from langchain_ollama import OllamaEmbeddings, ChatOllama
 from ragas.llms import LangchainLLMWrapper
 from ragas.embeddings import LangchainEmbeddingsWrapper
 from ragas.testset import TestsetGenerator
-
+from ragas.evaluation import EvaluationDataset
 import pandas as pd
 
 data_path = "./data"
+llama_url = "http://localhost:11434/"
 llama_model = "llama3.1:latest"
-llm = ChatOllama(model=llama_model)
-llama_embeddings = OllamaEmbeddings(model=llama_model)
+llm = ChatOllama(model=llama_model, base_url=llama_url)
+llama_embeddings = OllamaEmbeddings(model=llama_model, base_url=llama_url)
 
 
 loader = DirectoryLoader(path=data_path)
 docs = loader.load()
 
+print("passed loading docs")
+print("#" * 40)
+
 gen_llm = LangchainLLMWrapper(llm)
 gen_embeddings = LangchainEmbeddingsWrapper(llama_embeddings)
 
+print("passed langcahin wrappers")
+print("#" * 40)
+
 generator = TestsetGenerator(llm=gen_llm, embedding_model=gen_embeddings)
+
+print("passed testset gen")
+print("#" * 40)
+
 dataset = generator.generate_with_langchain_docs(docs, testset_size=3)
 
 df = pd.DataFrame(dataset)
