ee994e208531f725a3e190d303a0d1c0f82ce727
antoniosousa1 <antoniosousa640@gmail.com>
2025-02-14 16:17:36 -0500

    finally got the systehtic testset generator working with deepeval

--- App/tests/deepeval_testing.py
+++ App/tests/deepeval_testing.py
@@ -1,26 +1,69 @@
 from deepeval.synthesizer import Synthesizer
 from deepeval.synthesizer.config import ContextConstructionConfig
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 
-"""
-deepeval set-local-model --model-name=llama3.1:latest \
-    --base-url="http://localhost:11434/v1/" \
-    --api-key="ollama"
+from deepeval.models import DeepEvalBaseLLM, DeepEvalBaseEmbeddingModel
 
-deepeval unset-local-model
+##########################################################
+class LlamaLLM(DeepEvalBaseLLM):
+    def __init__(self):
+        pass
+
+    def get_model_name(self):
+        return "llama3.1:latest"
+    
+    def load_model(self):
+        return ChatOllama(
+            model=self.get_model_name()
+        )
+    
+    def generate(self, prompt: str) -> str:
+        chat_model = self.load_model()
+        return chat_model.invoke(prompt).content
+        
+    async def a_generate(self, prompt: str) -> str:
+        return self.generate(prompt)
+    
+##########################################################
+class LlamaLLMEmbeddings(DeepEvalBaseEmbeddingModel):
+    def __init__(self):
+        pass
+
+    def get_model_name(self):
+        return "llama3.1:latest"
+    
+    def load_model(self):
+        return OllamaEmbeddings(
+            model=self.get_model_name()
+        )
+    
+    def embed_text(self, text: str) -> list[float]:
+        embedding_model = self.load_model()
+        return embedding_model.embed_query(text)
+
+    def embed_texts(self, texts: list[str]) -> list[list[float]]:
+        embedding_model = self.load_model()
+        return embedding_model.embed_documents(texts)
+
+    async def a_embed_text(self, text: str) -> list[float]:
+        embedding_model = self.load_model()
+        return await embedding_model.aembed_query(text)
+
+    async def a_embed_texts(self, texts: list[str]) -> list[list[float]]:
+        embedding_model = self.load_model()
+        return await embedding_model.aembed_documents(texts)
+
+##########################################################
 
 
-deepeval set-local-embeddings --model-name=llama3.1:latest \
-    --base-url="http://localhost:11434/v1/" \
-    --api-key="ollama"
+llama_llm = LlamaLLM()
+llama_embeddings = LlamaLLMEmbeddings()
 
-deepeval unset-local-embeddings
-"""
-
-
-synthesizer = Synthesizer()
-
-#requires minium of 3 chunks so you must adjust chunk size accordingly
-synthesizer.generate_goldens_from_docs(document_paths=["./data/dog.txt"], context_construction_config=ContextConstructionConfig(chunk_size=50))
-
-
-print(synthesizer.synthetic_goldens)
+synthesizer = Synthesizer(model=llama_llm)
+synthesizer.generate_goldens_from_docs(document_paths=["./data/dog.txt"], context_construction_config=ContextConstructionConfig(embedder=llama_embeddings, chunk_size=50, critic_model=llama_llm))
+for i in range(len(synthesizer.synthetic_goldens)):
+    print("#" * 40)
+    print(f"Golden: {i}")
+    for x in synthesizer.synthetic_goldens[i]:
+        print(x)
+    print("#" * 40)
