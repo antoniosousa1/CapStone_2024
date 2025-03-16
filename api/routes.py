741eb0121e39baebd7e4b8ac88ced74ee66dcd76
antoniosousa1 <antoniosousa640@gmail.com>
2025-03-16 17:26:36 -0400

    just moved app intianite

--- api/routes.py
+++ api/routes.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 from llm_package.rag import Rag
 from langchain_ollama import OllamaLLM
 
+# intinaiate flask app
+app = Flask(__name__)
+
 # Initialize the Ollama LLM and DeepSeek LLM and embeddings
 deepseek_model = "llama3.1:70b"
 llm1 = OllamaLLM(model=deepseek_model)
 rag = Rag()
-
-# intinaiate flask app
-app = Flask(__name__)
 
 # API endpoint to return llm response before rag
 @app.route('/query', methods=['POST'])
