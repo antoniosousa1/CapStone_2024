6bc9a26d1ce02b59d70808177a7f1a723f8c35bf
Antonio Sousa <139799877+antoniosousa1@users.noreply.github.com>
2025-04-04 16:30:28 -0400

    Feature add docs integration (#28)
    * added to requiremnets
    
    * upload docs now uploads docs to database

--- backend/api_routes.py
+++ backend/api_routes.py
@@ -5,6 +5,7 @@ from llm_package import collection_manger
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 import os
@@ -24,6 +25,7 @@ embeddings = OllamaEmbeddings(model=llama_model)
 
 # intinaiate flask app
 app = Flask(__name__)
+CORS(app)
 
 # Initialize the Ollama LLM and DeepSeek LLM and embeddings
 
@@ -56,22 +58,18 @@ def llm_response():
 
 @app.route("/add", methods=["POST"])
 def upload_file():
-    if "file" not in request.files:
-        return {"error": "No file part"}, 400
+    if "files" not in request.files:
+        return {"error": "No files part"}, 400
 
-    file = request.files["file"]  # This retrieves the uploaded file
+    files = request.files.getlist("files")  # This retrieves the uploaded file
 
-    loaded_doc = doc_manager.load_doc(file)
+    loaded_docs = doc_manager.load_docs(files)
 
-    print("#" * 40 + "\n")
-    print(loaded_doc)
-    print("#" * 40 + "\n")
-
-    splits = doc_manager.split_doc(loaded_doc)
+    splits = doc_manager.split_docs(loaded_docs)
     
     collection_manger.add_docs_to_collection(splits=splits)
 
-    return {"message": f"File {file.filename} uploaded successfully!"}, 200
+    return {"message": f"Files uploaded successfully!"}, 200
 
 
 @app.route("/list-files", methods=["GET"])
